590bf72d7ca98832870d14aac0f90aed3ab79fa1
nipa <nipa@codefx.org>
2016-08-14 17:24:14 +0200

    Show all notes with Meta-Shift-N

--- src/workflowyfx.js
+++ src/workflowyfx.js
@@ -1,1 +1,30 @@
-console.log("Hello, WorkFlowy!")
+'use strict';
+
+console.log('Hello, WorkFlowy!')
+
+// SHOW ALL NOTES
+
+ /* TODO
+  *  - jQuery
+  *  - more sensible key code (?)
+  */
+
+const style = document.createElement('style')
+style.innerHTML = `
+    .show-all-notes .noted > .notes > .content {
+      display: block;
+      height: auto;
+      overflow: visible;
+    }`
+
+// $(style).appendTo(document.head)
+document.head.appendChild(style)
+
+//$(document.body).keypress(event => {
+//    if (event.keyCode == 13 && event.ctrlKey && event.shiftKey)
+//        $(document.body).toggleClass('show-all-notes')
+//})
+document.body.addEventListener('keypress', event => {
+    if (event.keyCode == 78 && event.shiftKey && event.metaKey)
+        document.body.classList.toggle('show-all-notes')
+})
