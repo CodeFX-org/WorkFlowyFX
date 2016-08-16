a69f1012d10c6c2f614bafd25c5f702855a4e893
nipa <nipa@codefx.org>
2016-08-16 16:04:25 +0200

    Add command to show all notes to shortcut list

--- src/workflowyfx.js
+++ src/workflowyfx.js
@@ -28,3 +28,29 @@ $(document.body).keypress(event => {
 	if (event.metaKey && event.shiftKey && event.keyCode == 78)
 		document.body.classList.toggle('show-all-notes')
 })
+// add it to shortcut list
+afterCommand('Add a note').add('Show/hide all notes', 'Meta + Shift + N')
+
+
+
+//+++++++++//
+// HELPERS //
+//+++++++++//
+
+function afterCommand(name) {
+	var $commandRow = $(`.shortcutList tr:contains(${name})`)
+	return {
+		add : function(name, description) {
+			afterRowAddNewCommand($commandRow, name, description)
+		}
+	}
+}
+
+function afterRowAddNewCommand($row, name, description) {
+	var newRow = `
+		<tr>
+			<td class="commandName">${name}</td>
+			<td class="commandDescription">${description}</td>
+		</tr>`
+	$row.after(newRow)
+}
