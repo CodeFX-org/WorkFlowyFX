8e51f299f8736e914b4a9583c57af19c261ad679
nipa <nipa@codefx.org>
2016-08-16 14:54:50 +0200

    Use jQuery to show all notes

--- src/workflowyfx.js
+++ src/workflowyfx.js
@@ -1,30 +1,18 @@
 'use strict';
+var $ = require('jquery')
 
 console.log('Hello, WorkFlowy!')
 
 // SHOW ALL NOTES
-
- /* TODO
-  *  - jQuery
-  *  - more sensible key code (?)
-  */
-
-const style = document.createElement('style')
-style.innerHTML = `
-    .show-all-notes .noted > .notes > .content {
-      display: block;
-      height: auto;
-      overflow: visible;
-    }`
-
-// $(style).appendTo(document.head)
-document.head.appendChild(style)
-
-//$(document.body).keypress(event => {
-//    if (event.keyCode == 13 && event.ctrlKey && event.shiftKey)
-//        $(document.body).toggleClass('show-all-notes')
-//})
-document.body.addEventListener('keypress', event => {
-    if (event.keyCode == 78 && event.shiftKey && event.metaKey)
-        document.body.classList.toggle('show-all-notes')
+$('<style>')
+	.text(`
+		.show-all-notes .noted > .notes > .content {
+			display: block;
+			height: auto;
+			overflow: visible;
+		}`)
+	.appendTo(document.head);
+$(document.body).keypress(event => {
+	if (event.keyCode == 78 && event.shiftKey && event.metaKey)
+		document.body.classList.toggle('show-all-notes')
 })
